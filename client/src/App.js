a8b432b236a6ea2437a8b27eafa562de69186eb6
Paul Scott <pausba@gmail.com>
2021-06-02 19:21:21 +0000

    welcome screen changed 2

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Pozdro ze Szkocji od Scona!
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
